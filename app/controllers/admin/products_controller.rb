b33b24ca2a82d19f6dd5d2b5b47df187b0adaa6b
罗泷 <godluos@luolongdeMacBook-Air.local>
2017-05-19 00:27:49 +0800

    add admin/products crdu

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,52 @@
+class Admin::ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path
+      flash[:notice] = "新增商品成功"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path
+      flash[:notice] = "修改商品成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path
+    flash[:alert] = "成功删除商品"
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :quantity)
+  end
+end
