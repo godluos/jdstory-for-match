651036d1e1dc3c94d86f3157ea2edb1a8d79f3d1
罗泷 <godluos@luolongdeMacBook-Air.local>
2017-06-09 15:09:49 +0800

    add user edit

--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -5,4 +5,24 @@ class Account::UsersController < ApplicationController
     @user = current_user
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to account_users_path
+      flash[:notice] = "个人资料修改成功"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :province, :city,
+    :district, :address, :tel)
+  end
 end
