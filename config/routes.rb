4a61b47568d901f8d078aecee02d42e92aa4a211
罗泷 <godluos@luolongdeMacBook-Air.local>
2017-06-09 17:08:51 +0800

    add user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ChinaCity::Engine => '/china_city'
   namespace :admin do
     resources :products
     resources :posts
